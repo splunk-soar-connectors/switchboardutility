8dc1c9951c719dcf4f15689eb39e781f0b4f7a15
Ian Rokas <grokas@splunk.com>
2025-03-18 07:30:42 +0000

    chore(ci): workflow updates and pre-commit changes and configurations added and applied
    [batch update]

--- README.md
+++ README.md
@@ -1,120 +1,105 @@
-[comment]: # "Auto-generated SOAR connector documentation"
 # Switchboard
 
-Publisher: Mhike  
-Connector Version: 1\.0\.1  
-Product Vendor: Mhike  
-Product Name: Switchboard  
-Product Version Supported (regex): "\.\*"  
-Minimum Product Version: 4\.9\.0  
+Publisher: Mhike \
+Connector Version: 1.0.1 \
+Product Vendor: Mhike \
+Product Name: Switchboard \
+Minimum Product Version: 4.9.0
 
 The Switchboard app manages various operations used in executing SOAR playbooks
 
+### Configuration variables
 
-----------------------------------------------------------------------------------------------------
-
-----------------------------------------------------------------------------------------------------
-
-Switchboard is an app designed to make executing the appropriate playbooks easier.  
-  
-The process for this execution depends on two things:
-
--   One active playbook that contains the switchboard **run playbooks** action
--   Specifically named playbooks to match on
-
-Executing the **run playbooks** accepts 3 inputs for playbook matching:
-
--   Repository name - (Required) Indicates which repository to match playbooks from
--   Product name - Accepts a string that represents the rule that generated the event
--   Rule name - Accepts a comma seperated string of product names to match on
-
-Types of named playbooks:
-
-1.  Rule: &ltexact rule name to match>
-    -   Matches if the literal rule name passed into **run playbooks** is the same
-2.  Product: &ltexact product name to match>
-    -   Matches if the playbook product name is a substring of the product name passed into **run
-        playbooks**
-3.  Subject: &ltsubstring to partially match against the rule name text>
-    -   Matches if the subject string is a substring of the rule name passed to **run playbooks**
-4.  Field: &ltexact field name to match>
-    -   Matches if the playbook's specified field name is in the original artifact in the container
-
-Once the set of matching playbooks is determined, those matching playbooks will be executed  
-  
-The matching playbooks and total count of executed playbooks are outputs of the action  
-  
-If no playbooks matched, no further playbooks will be executed by **run playbooks**
-
-----------------------------------------------------------------------------------------------------
-
-----------------------------------------------------------------------------------------------------
-
-
-### Configuration Variables
-The below configuration variables are required for this Connector to operate.  These variables are specified when configuring a Switchboard asset in SOAR.
+This table lists the configuration variables required to operate Switchboard. These variables are specified when configuring a Switchboard asset in Splunk SOAR.
 
 VARIABLE | REQUIRED | TYPE | DESCRIPTION
 -------- | -------- | ---- | -----------
-**https\_port** |  optional  | string | Splunk SOAR HTTPS port if your instance uses one other than 443
-**repository\_filter** |  optional  | string | A comma separated list of repositories that shouldn't be added to playbook cache
-**cache\_expiration** |  required  | numeric | How long should the playbook cache be valid in seconds
-**debug** |  optional  | boolean | Print debugging statements to log
+**https_port** | optional | string | Splunk SOAR HTTPS port if your instance uses one other than 443 |
+**repository_filter** | optional | string | A comma separated list of repositories that shouldn't be added to playbook cache |
+**cache_expiration** | required | numeric | How long should the playbook cache be valid in seconds |
+**debug** | optional | boolean | Print debugging statements to log |
 
-### Supported Actions  
-[test connectivity](#action-test-connectivity) - Validate connectivity to the Splunk SOAR instance  
-[run playbooks](#action-run-playbooks) - Identify and execute matching playbooks  
-[on poll](#action-on-poll) - Update the cached playbooks used by switchboard  
+### Supported Actions
+
+[test connectivity](#action-test-connectivity) - Validate connectivity to the Splunk SOAR instance \
+[run playbooks](#action-run-playbooks) - Identify and execute matching playbooks \
+[on poll](#action-on-poll) - Update the cached playbooks used by switchboard
 
 ## action: 'test connectivity'
+
 Validate connectivity to the Splunk SOAR instance
 
-Type: **test**  
+Type: **test** \
 Read only: **True**
 
 #### Action Parameters
+
 No parameters are required for this action
 
 #### Action Output
-No Output  
+
+No Output
 
 ## action: 'run playbooks'
+
 Identify and execute matching playbooks
 
-Type: **generic**  
+Type: **generic** \
 Read only: **False**
 
-Based on the standard naming conventions used by the switchboard app, run all matching playbooks in the playbook cache\.
+Based on the standard naming conventions used by the switchboard app, run all matching playbooks in the playbook cache.
 
 #### Action Parameters
+
 PARAMETER | REQUIRED | DESCRIPTION | TYPE | CONTAINS
 --------- | -------- | ----------- | ---- | --------
-**repository\_name** |  required  | The name of the repository to execute playbooks from | string | 
-**product\_name** |  optional  | Product strings to match on | string | 
-**rule\_name** |  optional  | Rule name to match on | string | 
+**repository_name** | required | The name of the repository to execute playbooks from | string | |
+**product_name** | optional | Product strings to match on | string | |
+**rule_name** | optional | Rule name to match on | string | |
 
 #### Action Output
-DATA PATH | TYPE | CONTAINS
---------- | ---- | --------
-action\_result\.status | string | 
-action\_result\.parameter\.product\_name | string | 
-action\_result\.parameter\.repository\_name | string | 
-action\_result\.parameter\.rule\_name | string | 
-action\_result\.data\.\*\.match\_found | boolean | 
-action\_result\.data\.\*\.matches | string | 
-action\_result\.summary | string | 
-action\_result\.message | string | 
-summary\.total\_objects | numeric | 
-summary\.total\_objects\_successful | numeric |   
+
+DATA PATH | TYPE | CONTAINS | EXAMPLE VALUES
+--------- | ---- | -------- | --------------
+action_result.status | string | | success failed |
+action_result.parameter.product_name | string | | |
+action_result.parameter.repository_name | string | | |
+action_result.parameter.rule_name | string | | |
+action_result.data.\*.match_found | boolean | | |
+action_result.data.\*.matches | string | | |
+action_result.summary | string | | |
+action_result.message | string | | |
+summary.total_objects | numeric | | |
+summary.total_objects_successful | numeric | | |
 
 ## action: 'on poll'
+
 Update the cached playbooks used by switchboard
 
-Type: **ingest**  
+Type: **ingest** \
 Read only: **False**
 
 #### Action Parameters
+
 No parameters are required for this action
 
 #### Action Output
+
 No Output
+
+______________________________________________________________________
+
+Auto-generated Splunk SOAR Connector documentation.
+
+Copyright 2025 Splunk Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
--- switchboard_connector.py
+++ switchboard_connector.py
@@ -1,3 +1,16 @@
+# Copyright (c) 2025 Splunk Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import datetime
 import json
 
@@ -6,20 +19,19 @@ from phantom.action_result import ActionResult
 
 
 class SwitchboardConnector(phantom.BaseConnector):
-
     is_polling_action = False
 
     def __init__(self):
-        super(SwitchboardConnector, self).__init__()
+        super().__init__()
         return
 
     def __print(self, value, is_debug):
         print_debug = False
         try:
-            print_debug = self.get_config()['debug']
+            print_debug = self.get_config()["debug"]
         except:
             pass
-        message = 'Failed to cast message to string'
+        message = "Failed to cast message to string"
         try:
             message = str(value)
         except:
@@ -32,14 +44,14 @@ class SwitchboardConnector(phantom.BaseConnector):
 
     def _get_rest_data(self, endpoint):
         try:
-            url = f'{self._get_base_url()}/{endpoint}'
+            url = f"{self._get_base_url()}/{endpoint}"
             self.__print(url, True)
             response = phantom.requests.get(url, verify=False, timeout=30)
             content = json.loads(response.text)
             code = response.status_code
             if 199 < code < 300:
-                if 'data' in content and 'container/' not in url:
-                    return content['data']
+                if "data" in content and "container/" not in url:
+                    return content["data"]
                 else:
                     return content
             else:
@@ -49,15 +61,15 @@ class SwitchboardConnector(phantom.BaseConnector):
 
     def _post_rest_data(self, endpoint, dictionary):
         try:
-            url = f'{self._get_base_url()}/{endpoint}'
+            url = f"{self._get_base_url()}/{endpoint}"
             self.__print(url, True)
             data = json.dumps(dictionary)
             response = phantom.requests.post(url, data=data, verify=False, timeout=30)
             content = response.text
             code = response.status_code
             if 199 < code < 300:
-                if 'data' in content:
-                    return content['data']
+                if "data" in content:
+                    return content["data"]
                 else:
                     return content
             else:
@@ -69,32 +81,32 @@ class SwitchboardConnector(phantom.BaseConnector):
         self.__print("_get_base_url()", True)
         port = 443
         try:
-            port = self.get_config()['https_port']
+            port = self.get_config()["https_port"]
         except:
             pass
-        return f'https://127.0.0.1:{port}'
+        return f"https://127.0.0.1:{port}"
 
     def _get_repository_id(self, scm):
-        self.__print(f'Start: _get_repository_id(): {datetime.datetime.now()}', True)
+        self.__print(f"Start: _get_repository_id(): {datetime.datetime.now()}", True)
         uri = f'rest/scm?_filter_name="{scm}"'
         response = self._get_rest_data(uri)
-        self.__print(f'Repository id for {scm} is {response[0]["id"]}', True)
-        self.__print(f'Finish: _get_repository_id(): {datetime.datetime.now()}', True)
-        return int(response[0]['id'])
+        self.__print(f"Repository id for {scm} is {response[0]['id']}", True)
+        self.__print(f"Finish: _get_repository_id(): {datetime.datetime.now()}", True)
+        return int(response[0]["id"])
 
     def _get_cef_keys(self):
-        self.__print(f'Start: _get_cef_content(): {datetime.datetime.now()}', True)
+        self.__print(f"Start: _get_cef_content(): {datetime.datetime.now()}", True)
         container_id = self.get_container_id()
-        uri = f'rest/artifact?_filter_container={container_id}&sort=id&order=asc&page_size=1'
+        uri = f"rest/artifact?_filter_container={container_id}&sort=id&order=asc&page_size=1"
         artifact = self._get_rest_data(uri)
         cef_keys = []
         if artifact:
-            for key in artifact[0]['cef'].keys():
+            for key in artifact[0]["cef"].keys():
                 cef_keys.append(key.lower())
         return cef_keys
 
     def _is_cache_valid(self, timestamp):
-        expiration = self.get_config()['cache_expiration']
+        expiration = self.get_config()["cache_expiration"]
         now = int((datetime.datetime.now() - datetime.datetime(1970, 1, 1)).total_seconds())
         if now - timestamp < expiration:
             return True
@@ -103,48 +115,48 @@ class SwitchboardConnector(phantom.BaseConnector):
     def _get_cache(self, repo_id, repo_name):
         self.load_state()
         state = self.get_state()
-        if self._is_cache_valid(state['timestamp']):
-            self.__print('State cache is still valid', False)
-            return state['cache'][repo_name]
+        if self._is_cache_valid(state["timestamp"]):
+            self.__print("State cache is still valid", False)
+            return state["cache"][repo_name]
         else:
-            self.__print('State cache is expired, re-caching', False)
+            self.__print("State cache is expired, re-caching", False)
             self._cache_playbooks()
-            return self._get_playbooks( repo_id, repo_name )
+            return self._get_playbooks(repo_id, repo_name)
 
     def _cache_playbooks(self):
-        self.__print(f'Start: _cache_playbooks(): {datetime.datetime.now()}', True)
+        self.__print(f"Start: _cache_playbooks(): {datetime.datetime.now()}", True)
         cache = {}
         try:
-            repo_filter = self.get_config()['repository_filter']
-            repo_filter = [repo.strip() for repo in repo_filter.split(',')]
+            repo_filter = self.get_config()["repository_filter"]
+            repo_filter = [repo.strip() for repo in repo_filter.split(",")]
             repo_filter = str(repo_filter).replace("'", '"')
-            uri = f'rest/scm?_exclude_name__in={repo_filter}'
+            uri = f"rest/scm?_exclude_name__in={repo_filter}"
             self.__print(uri, True)
             repositories = self._get_rest_data(uri)
             for repository in repositories:
                 self.__print(json.dumps(repository), True)
-                cache[repository['name']] = self._get_playbooks(repository['id'], repository['name'])
+                cache[repository["name"]] = self._get_playbooks(repository["id"], repository["name"])
             state = {}
-            state['cache'] = cache
-            state['timestamp'] = int((datetime.datetime.now() - datetime.datetime(1970, 1, 1)).total_seconds())
+            state["cache"] = cache
+            state["timestamp"] = int((datetime.datetime.now() - datetime.datetime(1970, 1, 1)).total_seconds())
             self.save_state(state)
-            self.__print(f'Finish: _cache_playbooks(): {datetime.datetime.now()}', True)
+            self.__print(f"Finish: _cache_playbooks(): {datetime.datetime.now()}", True)
             return phantom.APP_SUCCESS
         except Exception as e:
             self.__print("Exception thrown", False)
             self.__print(e, False)
 
     def _get_playbooks(self, repo_id, repo_name):
-        self.__print(f'Start: _get_playbooks(): {datetime.datetime.now()}', True)
+        self.__print(f"Start: _get_playbooks(): {datetime.datetime.now()}", True)
         playbook_endpoint = f'rest/playbook?_filter_scm={repo_id}&_filter_name__contains=": "&page_size=0'
         playbook_data = self._get_rest_data(playbook_endpoint)
         lookup = {}
         for playbook in playbook_data:
-            if ": " in playbook['name']:
+            if ": " in playbook["name"]:
                 try:
-                    cut_index = playbook['name'].index(': ')
-                    type_key = playbook['name'][:cut_index]
-                    name = playbook['name'][cut_index + 2:]
+                    cut_index = playbook["name"].index(": ")
+                    type_key = playbook["name"][:cut_index]
+                    name = playbook["name"][cut_index + 2 :]
                     if type_key in lookup:
                         lookup[type_key].append(name)
                     else:
@@ -153,98 +165,98 @@ class SwitchboardConnector(phantom.BaseConnector):
                 except:
                     pass
         self.__print(str(lookup), True)
-        self.__print(f'Finish: _get_playbooks(): {datetime.datetime.now()}', True)
+        self.__print(f"Finish: _get_playbooks(): {datetime.datetime.now()}", True)
         return lookup
 
     def _run_playbook(self, playbook_string):
-        self.__print('_run_playbook()', True)
+        self.__print("_run_playbook()", True)
         success = False
-        uri = 'rest/playbook_run'
+        uri = "rest/playbook_run"
         data = {"container_id": self.get_container_id(), "playbook_id": playbook_string, "scope": "all", "run": "true"}
         if self._post_rest_data(uri, data) is not None:
             success = True
         return success
 
     def _handle_run_playbooks(self, param):
-        self.__print(f'Start: _execute_playbooks(): {datetime.datetime.now()}', True)
+        self.__print(f"Start: _execute_playbooks(): {datetime.datetime.now()}", True)
         action_result = self.add_action_result(ActionResult(dict(param)))
-        rule = param.get('rule_name')
-        product = param.get('product_name')
-        scm = param.get('repository_name')
+        rule = param.get("rule_name")
+        product = param.get("product_name")
+        scm = param.get("repository_name")
         repo_id = self._get_repository_id(scm)
         executed_playbooks = []
         try:
             playbooks = self._get_cache(repo_id, scm)
-            if 'Rule' in playbooks:
-                for playbook in playbooks['Rule']:
+            if "Rule" in playbooks:
+                for playbook in playbooks["Rule"]:
                     if rule.lower() == playbook.lower():
-                        playbook_name = f'{scm}/Rule: {playbook}'
+                        playbook_name = f"{scm}/Rule: {playbook}"
                         if playbook_name not in executed_playbooks:
                             self.__print(playbook_name, True)
                             executed_playbooks.append(playbook_name)
                             self._run_playbook(playbook_name)
-            if 'Subject' in playbooks:
-                for playbook in playbooks['Subject']:
+            if "Subject" in playbooks:
+                for playbook in playbooks["Subject"]:
                     if playbook.lower() in rule.lower():
-                        playbook_name = f'{scm}/Subject: {playbook}'
+                        playbook_name = f"{scm}/Subject: {playbook}"
                         if playbook_name not in executed_playbooks:
                             self.__print(playbook_name, True)
                             executed_playbooks.append(playbook_name)
                             self._run_playbook(playbook_name)
-            if 'Product' in playbooks and product is not None and product != "":
+            if "Product" in playbooks and product is not None and product != "":
                 products = []
-                if ',' in product:
-                    for value in product.split(','):
+                if "," in product:
+                    for value in product.split(","):
                         products.append(value.strip())
                 else:
                     products.append(product)
                 for value in products:
                     if value:
-                        for playbook in playbooks['Product']:
+                        for playbook in playbooks["Product"]:
                             if value.strip().lower() in playbook.lower():
-                                playbook_name = f'{scm}/Product: {playbook}'
+                                playbook_name = f"{scm}/Product: {playbook}"
                                 if playbook_name not in executed_playbooks:
                                     self.__print(playbook_name, True)
                                     executed_playbooks.append(playbook_name)
                                     self._run_playbook(playbook_name)
-            if 'Field' in playbooks:
+            if "Field" in playbooks:
                 cef_keys = self._get_cef_keys()
-                for playbook in playbooks['Field']:
+                for playbook in playbooks["Field"]:
                     if playbook.lower() in cef_keys:
-                        playbook_name = f'{scm}/Field: {playbook}'
+                        playbook_name = f"{scm}/Field: {playbook}"
                         if playbook_name not in executed_playbooks:
                             self.__print(playbook_name, True)
                             executed_playbooks.append(playbook_name)
                             self._run_playbook(playbook_name)
             if executed_playbooks != []:
-                action_result.add_data({'match_found': True})
-                action_result.add_data({'matches': json.dumps(executed_playbooks)})
+                action_result.add_data({"match_found": True})
+                action_result.add_data({"matches": json.dumps(executed_playbooks)})
             else:
-                action_result.add_data({'match_found': False})
-                action_result.add_data({'matches': None})
-            self.__print(f'{len(executed_playbooks)} playbooks executed', False)
-            action_result.set_status(phantom.APP_SUCCESS, f'Successfully executed {len(executed_playbooks)} playbooks')
-            self.__print(f'Finish: _execute_playbooks(): {datetime.datetime.now()}', True)
+                action_result.add_data({"match_found": False})
+                action_result.add_data({"matches": None})
+            self.__print(f"{len(executed_playbooks)} playbooks executed", False)
+            action_result.set_status(phantom.APP_SUCCESS, f"Successfully executed {len(executed_playbooks)} playbooks")
+            self.__print(f"Finish: _execute_playbooks(): {datetime.datetime.now()}", True)
             return phantom.APP_SUCCESS
         except Exception as e:
-            self.set_status(phantom.APP_ERROR, 'Exception while running playbooks')
+            self.set_status(phantom.APP_ERROR, "Exception while running playbooks")
             self.__print(e, False)
-            action_result.set_status(phantom.APP_ERROR, 'Error processing playbooks')
+            action_result.set_status(phantom.APP_ERROR, "Error processing playbooks")
             return phantom.APP_ERROR
 
     def _handle_on_poll(self, param):
-        self.__print('start _on_poll()', True)
+        self.__print("start _on_poll()", True)
         action_result = self.add_action_result(ActionResult(dict(param)))
         self.is_polling_action = True
         try:
             self._cache_playbooks()
-            self.__print('Poll completed successfully', False)
-            action_result.set_status(phantom.APP_SUCCESS, 'Poll completed successfully')
+            self.__print("Poll completed successfully", False)
+            action_result.set_status(phantom.APP_SUCCESS, "Poll completed successfully")
             return phantom.APP_SUCCESS
         except Exception as e:
-            self.__print('Poll failed with an exception', False)
+            self.__print("Poll failed with an exception", False)
             self.__print(e, False)
-            action_result.set_status(phantom.APP_ERROR, 'Poll failed with an exception')
+            action_result.set_status(phantom.APP_ERROR, "Poll failed with an exception")
             return phantom.APP_ERROR
 
     def _handle_test_connectivity(self, param):
@@ -253,11 +265,11 @@ class SwitchboardConnector(phantom.BaseConnector):
         self.load_state()
         state = self.get_state()
         try:
-            self.__print(state['cache'], False)
+            self.__print(state["cache"], False)
         except:
-            self.__print('State file does not contain a playbook cache yet. Turn on polling to cache playbooks', False)
-        test_url = f'{self._get_base_url()}/rest/version'
-        self.__print(f'Attempting HTTP GET for {test_url}', False)
+            self.__print("State file does not contain a playbook cache yet. Turn on polling to cache playbooks", False)
+        test_url = f"{self._get_base_url()}/rest/version"
+        self.__print(f"Attempting HTTP GET for {test_url}", False)
         response = None
         try:
             response = phantom.requests.get(test_url, verify=False, timeout=30)
@@ -265,14 +277,14 @@ class SwitchboardConnector(phantom.BaseConnector):
         except:
             pass
         if response and 199 < response.status_code < 300:
-            version = json.loads(response.text)['version']
-            self.__print(f'Successfully retrieved platform version: {version}', False)
-            self.__print('Passed connection test', False)
+            version = json.loads(response.text)["version"]
+            self.__print(f"Successfully retrieved platform version: {version}", False)
+            self.__print("Passed connection test", False)
             return action_result.set_status(phantom.APP_SUCCESS)
         else:
-            self.__print(f'Failed to reach test url: {test_url}\nCheck your hostname config value', False)
-            self.__print('Failed connection test', False)
-            return action_result.set_status(phantom.APP_ERROR, f'Failed to reach test url {test_url}')
+            self.__print(f"Failed to reach test url: {test_url}\nCheck your hostname config value", False)
+            self.__print("Failed connection test", False)
+            return action_result.set_status(phantom.APP_ERROR, f"Failed to reach test url {test_url}")
 
     def handle_action(self, param):
         ret_val = phantom.APP_SUCCESS
@@ -281,13 +293,13 @@ class SwitchboardConnector(phantom.BaseConnector):
 
         self.__print(self.get_action_identifier(), True)
 
-        if action_id == 'run_playbooks':
+        if action_id == "run_playbooks":
             ret_val = self._handle_run_playbooks(param)
 
-        if action_id == 'on_poll':
+        if action_id == "on_poll":
             ret_val = self._handle_on_poll(param)
 
-        if action_id == 'test_connectivity':
+        if action_id == "test_connectivity":
             ret_val = self._handle_test_connectivity(param)
 
         return ret_val
